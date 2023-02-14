b5c011048f3e426a6c69f8706ed04209a4b3c62e
Graham Troy <berridgg@ad.unc.edu>
2023-02-14 15:23:31 -0500

    Step one attempt 1

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,14 @@
+"""EX02-One Shot Wordle - Loops!"""
+
+___author___ = "730560158"
+correct_word = "python"
+word = "What is your 6 letter guess?"
+
+while len(word) != 6:
+ print("That was not 6 letters! Try again:")
+while len(word) == 6: 
+  if word == correct_word: 
+    print("Woo! You got it!")
+  else:
+    print("Not quite. Play again soon!")
+
