0b5a2c643c7c804110531d4278ddec1128fefe79
Graham Troy <berridgg@ad.unc.edu>
2023-02-14 15:42:31 -0500

    minor fix to line 5

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,11 +2,11 @@
 
 ___author___ = "730560158"
 correct_word = "python"
-word = "What is your 6 letter guess?"
+word = input("What is your 6 letter guess?")
 
 if len(word) != 6:
  print("That was not 6 letters! Try again:")
-while len(word) == 6: 
+if len(word) == 6: 
   if word == correct_word: 
     print("Woo! You got it!")
   else:
