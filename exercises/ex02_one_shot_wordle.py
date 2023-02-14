0964d47297970668aefd335fed93c87db02c4176
Graham Troy <berridgg@ad.unc.edu>
2023-02-14 16:10:57 -0500

    added spaces for  formatting

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,15 +2,15 @@
 
 ___author___ = "730560158"
 correct_word = "python"
-word = input("What is your 6 letter guess?")
+word = input("What is your 6 letter guess? ")
 playing: bool = True 
 
 while playing:
  if len(word) != 6:
-  word = input("That was not 6 letters! Try again:")
+  word = input("That was not 6 letters! Try again: ")
  if len(word) == 6: 
    if word == correct_word: 
-     print("Woo! You got it!")
+     print("Woo! You got it! ")
      playing = False
    else:
      print("Not quite. Play again soon!")
