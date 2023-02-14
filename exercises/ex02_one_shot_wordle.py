8b2762410c9158dd65612d350e151068bef61403
Graham Troy <berridgg@ad.unc.edu>
2023-02-14 17:07:19 -0500

    adding in variables to step 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -6,11 +6,16 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 word = input("What is your 6 letter guess? ")
-word_index = word[0]
+count = 0
+word_index = word[count]
 box_storer = ""
 playing: bool = True 
 
 while playing:
+ while word_index >= len(correct_word): 
+    if word_index == correct_word[count]: 
+        append
+    count = count+1
  if len(word) != 6:
   word = input("That was not 6 letters! Try again: ")
  if len(word) == 6: 
