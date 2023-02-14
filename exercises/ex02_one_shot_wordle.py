16c822af7630d2eb30f778517741ac5f61ceab27
Graham Troy <berridgg@ad.unc.edu>
2023-02-14 17:34:42 -0500

    minor change to solve issue

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -13,7 +13,7 @@ playing: bool = True
 
 while playing:
  while count <= len(correct_word): 
-    if word[count] == correct_word[count]: 
+    if word[0] == correct_word[0]: 
         box_storer + GREEN_BOX
     else:
         box_storer + WHITE_BOX
