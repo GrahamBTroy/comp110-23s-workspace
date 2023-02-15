776ab3dfdcba88746524f6e7301930ec07a8aff1
Graham Troy <berridgg@ad.unc.edu>
2023-02-15 18:18:57 -0500

    trying to solve new problem

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,6 +1,5 @@
-"""EX02-One Shot Wordle - Loops!"""
-
 ___author___ = "730560158"
+"""EX02-One Shot Wordle - Loops!"""
 correct_word = "python"
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
@@ -15,31 +14,33 @@ count: int = 0
 word_index = word[count]
 box_storer = ""
 playing: bool = True 
-Alternative_placement: bool =  False
 char_count = 0
 
-while playing:
- while count < len(correct_word): 
-    if word[count] == correct_word[count]: 
-        box_storer += GREEN_BOX
-    else:
-        while not Alternative_placement and char_count < len(correct_word):
-            if word[char_count] == correct_word[count]:
-                Alternative_placement = True
-            else:
-                char_count = char_count + 1 
-        if Alternative_placement: 
-            box_storer += YELLOW_BOX
-        else: 
-            box_storer += WHITE_BOX
-    count = count + 1
- if len(word) == length: 
-   if word == correct_word:  
-     print(box_storer)
-     print("Woo! You got it! ")
-     playing = False
-   else:
-     print(box_storer)
-     print("Not quite. Play again soon!")
- playing = False
+while playing: 
+    while count < len(correct_word): 
+        if word[count] == correct_word[count]: 
+            box_storer += GREEN_BOX
+        else:
+            alternative_placement: bool =  False
+            while not alternative_placement and char_count < len(correct_word):
+                if word[char_count] == correct_word[count]:
+                    alternative_placement = True
+                else:
+                    char_count = char_count + 1 
+            if alternative_placement: 
+                box_storer += YELLOW_BOX
+            else: 
+                box_storer += WHITE_BOX
+            count = count + 1
+        playing = False
+    if len(word) == length: 
+        if word == correct_word:  
+            print(box_storer)
+            print("Woo! You got it! ")
+            playing = False
+        else:
+            print(box_storer)
+            print("Not quite. Play again soon!")
+            playing = False
+
 
