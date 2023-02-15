20c0c520e02899f892c6ab2b3883f430d41ac914
Graham Troy <berridgg@ad.unc.edu>
2023-02-15 15:26:18 -0500

    adding "length" variable

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -5,10 +5,9 @@ correct_word = "python"
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-
-
-word = input("What is your 6 letter guess? ")
-while len(word) != 6:
+length = 6
+word = input("What is your " + length + " letter guess? ")
+while len(word) != length:
     word = input("That was not 6 letters! Try again: ")
 
 count = 0
@@ -19,11 +18,11 @@ playing: bool = True
 while playing:
  while count < len(correct_word): 
     if word[count] == correct_word[count]: 
-        box_storer + GREEN_BOX
+        box_storer += GREEN_BOX
     else:
-        box_storer + WHITE_BOX
-    count = count+1
- if len(word) == 6: 
+        box_storer += WHITE_BOX
+    count = count + 1
+ if len(word) == length: 
    if word == correct_word:  
      print(box_storer)
      print("Woo! You got it! ")
