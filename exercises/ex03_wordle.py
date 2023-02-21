e662bbd22b972f1a20a3254160b5d2d20f0a1028
Graham Troy <berridgg@ad.unc.edu>
2023-02-21 14:33:10 -0500

    Continuing work on part 1

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,2 +1,7 @@
 """EX03 - Structured Wordle"""
 __author__ = "730561058"
+contains_character: bool = False
+inputed_word: str = ""
+inputed_letter: str = ""
+# setting up variables
+assert len(inputed_letter) == 1
