4e5fd0a5266cd42ee39882b3a5e5420b6b717b45
Graham Troy <berridgg@ad.unc.edu>
2023-02-21 15:24:18 -0500

    Minor fix

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -3,7 +3,7 @@ __author__ = "730561058"
 count: int = 0
 def contains_char(inputed_letter: str, inputed_word: str)-> bool:
   assert len(inputed_letter) == 1
-  while count <= len(inputed_word):
+  while count <= len(inputed_letter):
    count = count + 1
    if inputed_letter == inputed_word[count]: 
        return True
