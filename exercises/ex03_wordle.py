4b790101c9f59e7bdffdf5a39fa121416dd12111
Graham Troy <berridgg@ad.unc.edu>
2023-02-21 15:29:30 -0500

    Another minor fix

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,7 +1,7 @@
 """EX03 - Structured Wordle"""
 __author__ = "730561058"
 count: int = 0
-def contains_char(inputed_letter: str, inputed_word: str)-> bool:
+def contains_char(inputed_word: str, inputed_letter: str)-> bool:
   assert len(inputed_letter) == 1
   while count <= len(inputed_letter):
    count = count + 1
